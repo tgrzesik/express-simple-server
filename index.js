aad6e4334f899afa0195f0cc45c72e4e991acf71
Konrad Głowacki <konrad.glowacki@wpengine.com>
2020-10-29 12:20:30 +0100

    Add endpoint to dipslay envs

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ server.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
+server.get('/envs', (req, res) => {
+  console.log(process.env);
+  res.send('Envs displayed in logs!');
+});
+
 server.listen(PORT, () => {
   console.log(`Application is listening at port ${PORT}`);
 });
