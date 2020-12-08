f91229df5aae9ec523278494e16996304981de92
Konrad Głowacki <konrad.glowacki@wpengine.com>
2020-12-08 10:36:46 +0100

    Add public folder

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+server.use(express.static('public'))
+
 server.get('/', (req, res) => {
   res.send('Hello Express!');
 });
