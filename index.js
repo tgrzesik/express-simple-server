c611fc8f3fb383bf833ed85cb195fcc383daea9c
Konrad Głowacki <konrad.glowacki@wpengine.com>
2021-02-02 13:59:42 +0100

    Add graphql request

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+const axios = require('axios');
 const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3300;
 
 server.use(express.static('public'));
 
-server.get('/', (req, res) => {
+server.get('/', (_req, res) => {
   res.send('Hello Express!');
+});
+
+server.get('/fetch-wordpress-graphql', async (_req, res) => {
+  const query = `
+    query QueryPosts {
+      posts {
+        nodes {
+          id
+          content
+          title
+          slug
+          featuredImage {
+            node {
+              mediaDetails {
+                sizes {
+                  sourceUrl
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await axios.post(process.env.GRAPHQL_API_URL, { query });
+  res.send(response.data.data.posts.nodes);
 });
 
 server.get('/envs', (req, res) => {
